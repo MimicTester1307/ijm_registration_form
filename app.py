fcfe123f9fcbd76577ab7a4eeac8da02d5ac7d50
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2021-10-27 15:15:21 +0000

    create method for handling form inputs

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index_page():
     return render_template("index.html")
+
+
+@app.route("/", methods=["POST"])
+def receive_data():
+    error = None
+    if request.method == "POST":
+        pass
 
 
 if __name__ == '__main__':
