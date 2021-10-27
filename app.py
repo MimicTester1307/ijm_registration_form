8089145592000a302e69c33c13109186e11d8c0a
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2021-10-27 14:17:39 +0000

    include 'return' for render_template

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    render_template("index.html")
+def index_page():
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
