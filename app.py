86e8d1ca19b6e73ff3c73a917704770d21c25a7d
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2021-11-02 19:56:33 +0000

    implement logic for writing to sheet

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 """ Setting up access to the Spreadsheet File """
 # Creating Scope for File Editing
-scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file"]
+SCOPE = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file"]
+CREDS = ServiceAccountCredentials.from_json_keyfile_name("arise-conference-2021-beff271a724d.json", SCOPE)
+CLIENT = gspread.authorize(CREDS)  # authorizing the gspread object using the credentials
 
-creds = ServiceAccountCredentials.from_json_keyfile_name("arise-conference-2021-beff271a724d.json", scope)
-client = gspread.authorize(creds)  # authorizing the gspread object using the credentials
+# Initializing the row index for the spreadsheet to 2 because the first row serves as the headers.
+row_index = 2
 
 """ Flask Server Section """
 app = Flask(__name__)
@@ -20,19 +22,24 @@ def index_page():
 
 @app.route("/", methods=["POST"])
 def receive_data():
-    error = None
     if request.method == "POST":
+        # Retrieving the values inputted into the form
         first_name = request.form.get("firstName")
         last_name = request.form.get("lastName")
         email = request.form.get("email")
         phone_number = request.form.get("phoneNumber")
         volunteer_type = request.form.get("volunteerType")
 
-        return f"{first_name} {last_name} {email} {phone_number} {volunteer_type}"
+        write_to_sheet([first_name, last_name, email, phone_number, volunteer_type])
 
 
 def write_to_sheet(iterable: list) -> None:
-    pass
+    global row_index
+
+    arise_conference = CLIENT.open("Arise Conference 2021 Volunteer Registration").sheet1
+    arise_conference.insert_row(iterable, row_index)    # inserting the iterable into the sheet as a row
+
+    row_index += 1
 
 
 if __name__ == '__main__':
