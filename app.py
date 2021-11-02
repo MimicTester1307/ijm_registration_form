6a1e91a91e3944c505152ea6d45ceec545f366be
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2021-11-02 21:36:42 +0000

    encapsulation

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template, request
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from dotenv import load_dotenv
+import os
+
+# Loading the environment variables
+load_dotenv()
+
+SCOPE1 = os.getenv("SCOPE1")
+SCOPE2 = os.getenv("SCOPE2")
+CLIENT_SECRETS = os.getenv("CLIENT_SECRETS")
+SPREADSHEET_URL = os.getenv("SPREADSHEET_URL")
 
 """ Setting up access to the Spreadsheet File """
 # Creating Scope for File Editing
-SCOPE = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file"]
-CREDS = ServiceAccountCredentials.from_json_keyfile_name("arise-conference-2021-beff271a724d.json", SCOPE)
+SCOPE = [SCOPE1, SCOPE2]
+CREDS = ServiceAccountCredentials.from_json_keyfile_name(CLIENT_SECRETS, SCOPE)
 CLIENT = gspread.authorize(CREDS)  # authorizing the gspread object using the credentials
 
 # Initializing the row index for the spreadsheet to 2 because the first row serves as the headers.
@@ -23,7 +33,6 @@ def index_page():
 @app.route("/", methods=["POST"])
 def receive_data():
     if request.method == "POST":
-
         # Retrieving the values inputted into the form
         first_name = request.form.get("firstName")
         last_name = request.form.get("lastName")
@@ -39,8 +48,7 @@ def receive_data():
 def write_to_sheet(iterable):
     global row_index
 
-    arise_conference = CLIENT.open_by_url(
-        "https://docs.google.com/spreadsheets/d/1IbNNh4IMtbPp7igSVHkXwc_bFlzfYRRSR8iAyIe35IE")
+    arise_conference = CLIENT.open_by_url(SPREADSHEET_URL)
 
     arise_conference.sheet1.insert_row(iterable, row_index)  # inserting the iterable into the sheet as a row
 
